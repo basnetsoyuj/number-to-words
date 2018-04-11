f45093bb01d5317b5830b1149687204433bf59ba
SoyujBasnet <bsoyuj@gmail.com>
2018-04-11 12:55:57 +0545

    first commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# Number To Word
+
+This simple python script converts an integer number to corresponding words.
+It can convert the numbers till 10 ^ 23 i.e Vigintillion to words.
+
+## Example
+```
+1078 -> One Thousand and Seventy Eight
+```
+
+## Running as script
+The ```converter.py``` file can be run independently as a script as :-
+<pythonInterpreter> converter.py <INTEGER>
---
+++ main.py
@@ -0,0 +1,18 @@
+from converter import *
+
+#Use the converter function in anyway you want
+# converter(number : int)-->str
+'''def get_check_input():
+    # Getting the required number from the user
+    try:
+        n = int(input("Enter the digits of integer you want to convert to words:"))
+        word = converter(n)
+        print(word)
+    #handle error if input string isnt an integer
+    except ValueError as e:
+        print("Please enter an integer\n")
+        get_check_input()
+
+get_check_input()'''
+for x in range(0,100000):
+    print(converter(x),x)
